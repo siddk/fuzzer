d7b440bf91ac5fd78d153ffff1c74442d470475a
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2017-07-06 16:21:22 -0400

    Add error handling code

--- fuzzer/fuzzer.py
+++ fuzzer/fuzzer.py
@@ -164,7 +164,7 @@ class Fuzzer(object):
             self.afl_dir          = shellphish_afl.afl_dir(self.os)
 
             # the path to AFL capable of calling driller
-            self.afl_path         = shellphish_afl.afl_bin(self.os)
+            self.afl_path         = "/usr/local/bin/afl-unix/afl-fuzz"
 
             if self.os == 'cgc':
                 self.afl_path_var = shellphish_afl.afl_path_var('cgc')
@@ -539,6 +539,8 @@ class Fuzzer(object):
         self.fuzz_id += 1
 
         outfile = os.path.join(self.job_dir, outfile)
+        l.warning('Outfile: %s' % outfile)
+        l.warning('Args: %s' % str(args))
         with open(outfile, "w") as fp:
             return subprocess.Popen(args, stdout=fp, close_fds=True)
 
